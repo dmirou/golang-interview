1ec5ad90a9e7800c7397a25f49d0e5c7b855df98
Dmitriy Neustroev <dmitriy.neustroev@gmail.com>
2021-12-04 20:57:01 +0700

    Add slice init

---
+++ compositetype/slice/init/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func printBytes(name string, b []byte) {
+	fmt.Printf("%s %T, p: %p, val: %v, str: %q, len: %d, cap: %d\n",
+		name, b, b, b, b, len(b), cap(b))
+}
+
+func main() {
+
+	var s01 []byte // nil
+	printBytes("s01", s01)
+
+	s02 := []byte(nil) //nil
+	printBytes("s02", s02)
+
+	var s03 = []byte{} // not nil
+	printBytes("s03", s03)
+
+	var base = []byte("hello мир")
+	printBytes("base", base)
+
+	sIdx := bytes.Index(base, []byte(" "))
+
+	s1 := base[sIdx+1:]
+	printBytes("s1", s1)
+
+	s2 := base[:sIdx]
+	printBytes("s2", s2)
+
+	s3 := s2[:len(s2)+3]
+	printBytes("s3", s3)
+
+	s3 = append(s3, []byte("иша")...)
+	printBytes("s3", s3)
+
+	var s4 = make([]byte, len(s3))
+	copy(s4, s3)
+	printBytes("s4", s4)
+
+	var s5 []byte
+	for _, v := range s4 {
+		printBytes("s5", s5)
+		s5 = append(s5, v)
+	}
+	printBytes("s5", s5)
+
+}
