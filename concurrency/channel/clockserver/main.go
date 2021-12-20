831e9fcb64b94d751d14eba9ba1d88f88e639d75
Dmitriy Neustroev <dmitriy.neustroev@gmail.com>
2021-12-20 17:10:07 +0700

    Allow to specify port

--- concurrency/channel/clockserver/main.go
+++ concurrency/channel/clockserver/main.go
@@ -5,11 +5,20 @@ import (
 	"io"
 	"log"
 	"net"
+	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
-	const port = 8181
+	if len(os.Args) < 2 {
+		log.Fatalf("can't get port number from args: %v", os.Args)
+	}
+
+	port, err := strconv.ParseInt(os.Args[1], 10, 32)
+	if err != nil {
+		log.Fatalf("can't parse input port %s: %v", os.Args[1], err)
+	}
 
 	addr := fmt.Sprintf("localhost:%d", port)
 	listener, err := net.Listen("tcp", addr)
@@ -18,7 +27,7 @@ func main() {
 	}
 	defer listener.Close()
 
-	fmt.Println("clock1: wait for connections")
+	fmt.Printf("clock1: wait for connections on %s\n", listener.Addr().String())
 
 	for {
 		conn, err := listener.Accept()
@@ -47,7 +56,7 @@ func handleCon(conn net.Conn) {
 		msg := time.Now().Format("15:04:05\n")
 		_, err := io.WriteString(conn, msg)
 		if err != nil {
-			fmt.Printf("handleCon: can't send to the client: %v", err)
+			fmt.Printf("handleCon: can't send to the client: %v\n", err)
 			return
 		}
 		fmt.Printf("handleCon: sent to the client: %s", msg)
