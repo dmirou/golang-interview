aa34fc7772366aaafcfdbfa324e3d9b542b2bbf7
Dmitriy Neustroev <dmitriy.neustroev@gmail.com>
2022-05-09 16:18:24 +0700

    Extend sentinel error handling

--- error/handling/sentinel/cmd/main.go
+++ error/handling/sentinel/cmd/main.go
@@ -3,18 +3,42 @@ package main
 import (
 	"errors"
 	"fmt"
-	"os"
 
 	"github.com/dmirou/learngo/error/handling/sentinel/internal/storage"
 )
 
-func main() {
+func TopLevelError() {
 	err := storage.AddPerson("")
 	// this package depends on the storage package because it uses his error
 	if errors.Is(err, storage.ErrInvalidName) {
-		fmt.Println("can not add person to the storage")
-		os.Exit(1)
+		fmt.Printf("can not add person to the storage: %v\n\n", err)
 	}
+}
 
-	fmt.Println("saved successfully")
+func ErrorInTheChain() {
+	name := "Ivan"
+	err := storage.AddPerson(name)
+	if err != nil {
+		if errors.Is(err, storage.ErrConnection) {
+			fmt.Println("ErrConnection found in the chain because %w format was used")
+		}
+		fmt.Printf("error in the chain: %v\n\n", err)
+	}
+}
+
+func ObfuscatedErrorInTheChain() {
+	name := "Ivan"
+	err := storage.DeletePerson(name)
+	if err != nil {
+		if !errors.Is(err, storage.ErrAlreadyDeleted) {
+			fmt.Println("ErrAlreadyDeleted not found in the chain because %v format was used")
+		}
+		fmt.Printf("obfuscated error: %v\n", err)
+	}
+}
+
+func main() {
+	TopLevelError()
+	ErrorInTheChain()
+	ObfuscatedErrorInTheChain()
 }
--- error/handling/sentinel/internal/storage/storage.go
+++ error/handling/sentinel/internal/storage/storage.go
@@ -1,9 +1,18 @@
 package storage
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // ErrInvalidName is part of public API of the package.
 var ErrInvalidName = errors.New("invalid name")
+
+// ErrConnection is part of public API of the package.
+var ErrConnection = errors.New("can not connect to the storage")
+
+// ErrAlreadyDeleted is part of public API of the package.
+var ErrAlreadyDeleted = errors.New("person already deleted")
 
 // AddPerson is a part of public API of the package.
 func AddPerson(name string) error {
@@ -11,6 +20,32 @@ func AddPerson(name string) error {
 		return ErrInvalidName
 	}
 
-	// add person to some storage
+	err := addToStorage()
+	if err != nil {
+		return fmt.Errorf("add person: %w", err)
+	}
+
 	return nil
+}
+
+// DeletePerson is a part of public API of the package.
+func DeletePerson(name string) error {
+	if name == "" {
+		return ErrInvalidName
+	}
+
+	err := deleteFromStorage()
+	if err != nil {
+		return fmt.Errorf("delete person: %v", err)
+	}
+
+	return nil
+}
+
+func addToStorage() error {
+	return ErrConnection
+}
+
+func deleteFromStorage() error {
+	return ErrAlreadyDeleted
 }
