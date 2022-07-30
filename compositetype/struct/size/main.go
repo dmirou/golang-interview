2f747d2fcb46f73ce0687d3a993bc266e7279757
Dmitriy Neustroev <dmitriy.neustroev@gmail.com>
2022-07-30 18:03:42 +0700

    Improve struct size example

--- compositetype/struct/size/main.go
+++ compositetype/struct/size/main.go
@@ -5,17 +5,24 @@ import (
 	"unsafe"
 )
 
-type Person struct {
-	isMarried bool  // 1 byte aligned to 8 bytes
-	age       int64 // 8 bytes aligned to 8 bytes
-	isMale    bool  // 1 byte aligned to 8 bytes
+type Person struct { // size 24 bytes, alignof 8 bytes
+	isMarried bool  // offset 0 bytes, size 1 byte
+	age       int64 // offset 8 bytes, size 8 bytes
+	isMale    bool  // offset 16 bytes, size 1 byte
 }
 
-type PersonOptimized struct {
-	age int64 // 8 bytes aligned to 8 bytes
+type PersonOptimized struct { // size 16 bytes, alignof 8 bytes
+	age int64 // offset 0 bytes, size 8 bytes
 
-	isMarried bool // 1 byte, not aligned
-	isMale    bool // 1 byte, aligned to 8 bytes together with isMarried
+	isMarried bool // offset 8 bytes, size 1 byte
+	isMale    bool // offest 9 bytes, size 1 byte
+}
+
+type Store struct { // size 48 bytes, alignof is 8 bytes
+	name       string // offset 0 bytes, size 16 bytes
+	address    string // offset 16 bytes, size 16 bytes
+	goodsCount int    // offset 32 bytes, size 8 bytes
+	soldCount  int    // offset 40 bytes, size 8 bytes
 }
 
 func main() {
@@ -34,4 +41,17 @@ func main() {
 	fmt.Printf("p2 %+v age offest is %v bytes\n", p2, unsafe.Offsetof(p2.age))
 	fmt.Printf("p2 %+v isMarried offest is %v bytes\n", p2, unsafe.Offsetof(p2.isMarried))
 	fmt.Printf("p2 %+v isMale offest is %v bytes\n", p2, unsafe.Offsetof(p2.isMale))
+
+	var s1 Store
+
+	fmt.Printf("s1 %+v size is %v bytes\n", s1, unsafe.Sizeof(s1))
+	fmt.Printf("s1.name %+v size is %v bytes\n", s1, unsafe.Sizeof(s1.name))
+	fmt.Printf("s1.address %+v size is %v bytes\n", s1, unsafe.Sizeof(s1.address))
+	fmt.Printf("s1.goodsCount %+v size is %v bytes\n", s1, unsafe.Sizeof(s1.goodsCount))
+	fmt.Printf("s1.soldCount %+v size is %v bytes\n", s1, unsafe.Sizeof(s1.soldCount))
+	fmt.Printf("s1 %+v alignof is %v bytes\n", s1, unsafe.Alignof(s1))
+	fmt.Printf("s1 %+v name offest is %v bytes\n", s1, unsafe.Offsetof(s1.name))
+	fmt.Printf("s1 %+v address offest is %v bytes\n", s1, unsafe.Offsetof(s1.address))
+	fmt.Printf("s1 %+v goodsCount offest is %v bytes\n", s1, unsafe.Offsetof(s1.goodsCount))
+	fmt.Printf("s1 %+v soldCount offest is %v bytes\n", s1, unsafe.Offsetof(s1.soldCount))
 }
